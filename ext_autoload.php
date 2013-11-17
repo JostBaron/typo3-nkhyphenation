5329f72a63820189008885b3f1210bf7ed5ca245
Jost Baron <jost@netzkoenig.de>
2013-11-17 15:40:25 +0100

    Removed unused classes and files.

--- ext_autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-$extensionClassesPath = t3lib_extMgm::extPath('nkhyphenation') . 'Classes/';
-
-return array(
-	'tx_nkhyphenation_utility_buildtrieutility' => $extensionClassesPath . 'Utility/BuildTrieUtility.php'
-);*/
