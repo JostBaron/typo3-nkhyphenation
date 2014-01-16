8fc537739c60e152e626533482efc5cea31b4dbf
Jost Baron <jost@netzkoenig.de>
2014-01-16 02:46:27 +0100

    [TASK] Added copyright headers to most files.

--- Resources/Public/JavaScript/sanitizeCopiedText.js
+++ Resources/Public/JavaScript/sanitizeCopiedText.js
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * Copyright notice
+ * (c) 2013 Jost Baron <j.baron@netzkoenig.de>
+ * All rights reserved
+ * 
+ * This file is part of the TYPO3 extension "nkhyphenation".
+ *
+ * The TYPO3 extension "nkhyphenation" is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * The TYPO3 extension "nkhyphenation" is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the TYPO3 extension "nkhyphenation".  If not, see
+ * <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 /**
  * Register to the copy and cut events.
