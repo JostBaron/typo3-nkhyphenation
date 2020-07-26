944e474e7fc42214410c2117fe54e832a71835bc
Jost Baron <j.baron@netzkoenig.de>
2020-07-26 12:10:45 +0200

    [BUGFIX] Fix TCA.

--- Configuration/TCA/tx_nkhyphenation_domain_model_hyphenationpatterns.php
+++ Configuration/TCA/tx_nkhyphenation_domain_model_hyphenationpatterns.php
@@ -29,19 +29,17 @@ $ll = 'LLL:EXT:nkhyphenation/Resources/Private/Language/locallang_db.xml:';
 
 $GLOBALS['TCA']['tx_nkhyphenation_domain_model_hyphenationpatterns'] = [
     'ctrl' => [
-        'ctrl' => [
-            'title' => 'LLL:EXT:nkhyphenation/Resources/Private/Language/locallang_db.xml:hyphenationpatterns_recordlabel',
-            'label' => 'title',
-            'tstamp'    => 'tstamp',
-            'crdate'    => 'crdate',
-            'cruser_id' => 'cruser_id',
-            'dividers2tabs' => 1,
-            'delete' => 'deleted',
-            'enablecolumns' => [
-                'disabled' => 'hidden',
-            ],
-            'searchFields' => 'uid,title',
+        'title' => 'LLL:EXT:nkhyphenation/Resources/Private/Language/locallang_db.xml:hyphenationpatterns_recordlabel',
+        'label' => 'title',
+        'tstamp'    => 'tstamp',
+        'crdate'    => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'dividers2tabs' => 1,
+        'delete' => 'deleted',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
         ],
+        'searchFields' => 'uid,title',
     ],
     'columns' => [
         'pid' => [
